cfce487ed3670b8ca4d38e3ccef31f0526a312e8
shuikouzhizi7@gmail.com <shuikouzhizi7@gmail.com>
2022-05-28 09:58:29 +0900

    プロジェクト ファイルを追加します。

---
+++ ensyuu1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ensyuu1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 10; i++)//初期化;条件式;変化式;
+            {
+                if(i == 5)
+                {
+                    MessageBox.Show(i + "");//実行する処理
+                    MessageBox.Show("ここで半分です");
+                }
+                else
+                {
+                    MessageBox.Show(i + "");
+                }
+            }
+        }
+    }
+}
