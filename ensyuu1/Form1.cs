6393f0a9f6a5f9c592aea0b2ec1451fa45a5aee9
shuikouzhizi7@gmail.com <shuikouzhizi7@gmail.com>
2022-05-28 10:03:59 +0900

    最後のコミット

--- ensyuu1/Form1.cs
+++ ensyuu1/Form1.cs
@@ -21,7 +21,7 @@ namespace ensyuu1
         {
             for(int i = 1; i <= 10; i++)//初期化;条件式;変化式;
             {
-                if(i == 5)//iが5だった場合の処理
+                if(i == 5)//変数iが5だった場合の処理
                 {
                     MessageBox.Show(i.ToString());//実行する処理
                     MessageBox.Show("ここで半分です");
